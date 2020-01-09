7ec3d31c1bb0e3651045ca230dcd78a294c0a8de
Julian Yates <julianyates@Julians-MacBook-Pro.local>
2020-01-09 10:01:40 -0600

    has weird error

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
+const helmet = require('helmet')
 
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('combined'));
 
 const authRouter = require('./auth/auth-router.js');
 server.use('/auth', authRouter);
+
+const jokesRouter = require('./jokes/jokes_router.js');
+server.use('/api', jokesRouter);
 
 const PORT = process.env.PORT || 4400;
 server.listen(PORT, () => console.log(`Server running on ${PORT}`))
